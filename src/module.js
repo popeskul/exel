dd3a04081cb695487fbbcdcc4371854c5f65ebdf
Pavel Popeskul <pavel.popeskul@softermii.com>
2020-09-13 14:21:45 +0300

    settings: webpack

--- src/module.js
+++
@@ -1,9 +0,0 @@
-console.log("1111");
-
-async function start() {
-  await Promise.resolve();
-}
-
-start();
-
-console.log("module.js");
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,34 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const isProd = process.env.NODE_ENV === "production";
+const isDev = !isProd;
+
+const filename = ext => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"]
+      }
+    }
+  ];
+
+  if (isDev) {
+    loaders.push("eslint-loader");
+  }
+
+  return loaders;
+};
+
 module.exports = {
   context: path.resolve(__dirname, "src"), // return string with absolute path
   mode: "development",
-  entry: "./index",
+  entry: ["@babel/polyfill", "./index"],
   output: {
     // hash - unique id
-    filename: "bundle.[hash].js",
+    filename: filename("js"),
     path: path.resolve(__dirname, "dist")
   },
   resolve: {
@@ -20,10 +41,21 @@ module.exports = {
       "@core": path.resolve(__dirname, "src/core")
     }
   },
+  devtool: isDev ? "source-map" : false,
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    hot: isDev
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "index.html"
+      template: "index.html",
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
     }),
     new CopyPlugin({
       patterns: [
@@ -34,7 +66,7 @@ module.exports = {
       ]
     }),
     new MiniCssExtractPlugin({
-      filename: "bundle.[hash].css"
+      filename: filename("css")
     })
   ],
   module: {
@@ -42,17 +74,22 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         // "use" run from right to left
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
       },
       {
         test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
+        use: jsLoaders(),
+        exclude: /node_modules/
       }
     ]
   }
